4819f6242e7babf775ffa84815eb3ea82b27ba92
Daniel Portella <dmportell@hotmail.com>
2021-06-14 17:21:21 +0100

    added order ticket

---
+++ evola/src/components/order-ticket/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class OrderTicket extends Component {
+  render() {
+    return (
+      <div>
+        Under Construction
+      </div>
+    );
+  }
+}
+
+export default OrderTicket;
