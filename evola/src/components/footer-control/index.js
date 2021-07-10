5878836b62e962336a13a63f5bc01d7f09b9b5ba
Daniel Portella <dmportell@hotmail.com>
2021-07-10 23:43:41 +0100

    added some info

--- evola/src/components/footer-control/index.js
+++ evola/src/components/footer-control/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function FooterControl() {
     return (
         <footer className="px-4 py-4 h-full bg-gray-600 text-white flex justify-centre">
-            Evola Deliveries is a Eve Online Corporation
+            <span className="font-bold">Evola Deliveries</span> is a Eve Online Corporation
         </footer>
     );
 
