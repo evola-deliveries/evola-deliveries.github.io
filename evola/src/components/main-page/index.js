961981c57cc38b75bb9427788147f5a08cb0912f
Yuri Lavelle <yurilavelle@gmail.com>
2022-08-18 17:58:59 +0100

    Order and mainpage update

--- evola/src/components/main-page/index.js
+++ evola/src/components/main-page/index.js
@@ -11,7 +11,7 @@ const MainPage = () => {
 		<Router>
 			<div className="bg-cover flex flex-col min-h-screen" style={
 				{
-					"backgroundImage": "url(https://images.pexels.com/photos/41951/solar-system-emergence-spitzer-telescope-telescope-41951.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260)",
+					"backgroundImage": "url(https://cdn.discordapp.com/attachments/844609518632435782/977965791498600498/evobgtest.png)",
 					"background-position": "center",
 					"background-repeat": "repeat",
 					"background-size": "cover"
--- evola/src/components/order-ticket/index.js
+++ evola/src/components/order-ticket/index.js
@@ -7,21 +7,12 @@ export default function ContractCreator({ outbound, inbound, pricing }) {
             <div class="p-1 border-2 border-black font-sans w-80 sm:w-72 bg-white">
                 <div class="flex justify-center text-4xl font-extrabold">Evola</div>
                 <div class="flex justify-center text-4xl font-extrabold">Deliveries</div>
-                <div class="flex justify-center leading-snug border-b-8 border-black">Eve Online Corporation</div>
-                <div class="flex justify-between font-bold border-black">
-                    <div>Contract's</div>
-                </div>
-                <div class="flex justify-between items-end font-extrabold">
-                    <div>
-                        <div class="text-4xl">Packages</div>
-                    </div>
-                    <div class="text-5xl">1</div>
-                </div>
-                <div class="border-t-4 border-black text-sm pb-1">
+                <div class="flex justify-center leading-snug border-b-4 border-black">Eve Online Corporation</div>
+                <div class="text-sm pb-1">
                     <hr class="border-gray-500" />
                     <div class="flex justify-between">
                         <div>
-                            <span class="font-bold">Package 1</span>
+                            <span class="font-bold">Package Details</span>
                         </div>
                         <span>{pricing && pricing.janice !== ""
                             ? <a href={pricing.janice !== "" && Config.janice_url + pricing.janice} target="_blank" rel="noreferrer" class="text-purple-900 font-semibold">Janice {pricing && pricing.janice}</a>
@@ -32,23 +23,19 @@ export default function ContractCreator({ outbound, inbound, pricing }) {
                         <span class="italic pl-4">Appraisal Reference</span>
                         <div><span class="select-all">{pricing.janice !== "" && pricing.janice}</span></div>
                     </div>
-                    <hr class="border-gray-500" />
+
                     <div class="flex justify-between">
                         <span class="italic pl-4">Cubic Meter's</span>
                         <div className={pricing && pricing.totals.volumeInvalid ? "font-bold text-red-600" : "font-bold text-green-600"}><span class="select-all">{pricing && Number(pricing.volume).toLocaleString('en')}</span> m3</div>
                     </div>
-                    <hr class="border-gray-500" />
-                    <div class="flex justify-between">
-                        <span class="italic pl-4">Jita Sell Value</span>
-                        <div className={pricing && pricing.totals.collateralInvalid ? "font-bold text-red-600" : "font-bold text-green-600"}><span class="select-all">{pricing && Number(pricing.price).toLocaleString('en')}</span> isk</div>
+
+
+<div class="flex justify-between">
+                        <div class="font-bold">Pricing</div>
+            
                     </div>
                     <hr class="border-gray-500" />
-                    <div class="flex justify-between">
-                        <div class="font-bold">{outbound && outbound.system}</div>
-                        <div>to</div>
-                        <div class="font-bold">{inbound && inbound.system}</div>
-                    </div>
-                    <div class="flex justify-between">
+<div class="flex justify-between">
                         <div class="pl-4">Volume</div>
                         <div className={pricing && pricing.totals.volumeInvalid ? "font-bold text-red" : ""}><span class="select-all">{inbound && Number(inbound.reward.volume).toLocaleString('en')}</span> m3</div>
                     </div>
@@ -81,31 +68,26 @@ export default function ContractCreator({ outbound, inbound, pricing }) {
                         </div>
                         <div class="font-bold text-green-600"><span class="select-all">{pricing && Number(pricing.totals.total).toLocaleString('en')}</span> isk</div>
                     </div>
-                    <hr class="border-gray-500" />
+
                     <div class="flex justify-between">
                         <div>
                             <span class="font-bold">Total Collateral</span>
                         </div>
                         <div className={pricing && pricing.totals.collateralInvalid ? "font-bold text-red-600" : "font-bold text-green-600"}><span class="select-all">{pricing && Number(pricing.price).toLocaleString('en')}</span> isk</div>
                     </div>
-                    <hr class="border-gray-500" />
-                    <div class="flex justify-between">
-                        <div class="pl-4">Volume Cost</div>
-                        <div><span class="select-all">{pricing && Number(pricing.totals.volume).toLocaleString('en')}</span> isk</div>
-                    </div>
-                    <hr class="border-gray-500" />
-                    <div class="flex justify-between">
-                        <div class="pl-4">Collateral Cost</div>
-                        <div><span class="select-all">{pricing && Number(pricing.totals.collateral).toLocaleString('en')}</span> isk</div>
-                    </div>
+
+
                 </div>
-                <div class="border-t-4 border-black pt-1 text-sm"></div>
+
                 <div class="flex justify-center"></div>
-                <div class="border-t-8 border-black flex leading-none text-xs pt-2 pb-1">
+                <div class="border-t-4 border-black flex leading-none text-xs pt-2 pb-1">
                     <div class="pr-1">*</div>
                     <div>
-                        Contracts are issued directly to <span class="font-bold">Evola Deliveries</span>. If you wish to use our services for a route that is currently not supported please contact <span class="font-bold">Nahtsu</span> directly.<br />
-                        If you have any feedback please let us know!
+                        Contracts are issued directly to: <br/>
+                        <span class="font-bold">Evola Deliveries</span>.<br/><br/>
+                        <span class="font-bold">No Containers!</span><br />
+                        <span class="font-bold">No Assembled Ships!</span><br/><br/>
+                        If you have any feedback please contact <span class="font-bold">Nahtsu#9654</span>
                     </div>
                 </div>
             </div>
