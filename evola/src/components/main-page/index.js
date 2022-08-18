a21779908433ebadea929dd37b20bedc3941e65d
Yuri Lavelle <yurilavelle@gmail.com>
2022-08-18 18:41:17 +0100

    Compressed Image

--- evola/src/components/main-page/index.js
+++ evola/src/components/main-page/index.js
@@ -11,7 +11,7 @@ const MainPage = () => {
 		<Router>
 			<div className="bg-cover flex flex-col min-h-screen" style={
 				{
-					"backgroundImage": "url(https://cdn.discordapp.com/attachments/844609518632435782/977965791498600498/evobgtest.png)",
+					"backgroundImage": "url(https://cdn.discordapp.com/attachments/376647838710628355/1009872730604179537/evobgtest-min.png)",
 					"background-position": "center",
 					"background-repeat": "repeat",
 					"background-size": "cover"
