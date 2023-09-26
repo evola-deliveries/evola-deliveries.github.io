67b9cfab94b3b030bbd0f2aceedf8743ffde8a13
42-Ametus <tcabel2017@hotmail.com>
2023-09-26 11:49:04 -0500

    bye

--- evola/src/components/contract-creator/index.js
+++ evola/src/components/contract-creator/index.js
@@ -78,43 +78,23 @@ export default function ContractCreator() {
                 <div className="shadow-lg rounded-lg bg-white px-2 py-2">
                     <div className="my-2">
                         <div className="headerCont">
-                            <h4 class="md:block text-2xl text-gray-400">WELCOME TO</h4>
-                            <h3 class="md:block font-bold text-3xl text-gray-700">EVOLA DELIVERIES</h3>
-                            <a class="flex items-baseline mt-3 text-3xl text-blue-600 hover:text-blue-900 focus:text-blue-900" href={ConfigService.discord_url} target="_blank" rel="noreferrer">
-                                <span>Join Discord</span>
+                            <h3 class="md:block font-bold text-3xl text-gray-700">Please make all contracts to MLL/Darkhorse. Thank you for your business</h3>
+                            <a class="flex items-baseline mt-3 text-3xl text-blue-600 hover:text-blue-900 focus:text-blue-900" href="https://discord.gg/tpQe3yevdz" target="_blank" rel="noreferrer">
+                                <span>Join Darkhorse</span>
+                                <span class="text-xl ml-1">&#x279c;</span>
+                            </a>
+                            <a class="flex items-baseline mt-3 text-3xl text-blue-600 hover:text-blue-900 focus:text-blue-900" href="https://discord.gg/fQqCvHT" target="_blank" rel="noreferrer">
+                                <span>Join MLL</span>
                                 <span class="text-xl ml-1">&#x279c;</span>
                             </a>
                         </div>
-                        <div className="headerCont2">
-                            <h4 class="md:block text-2xl text-gray-400">Queue Status:</h4>
-                            <h3 class="font-bold text-2xl inLine outstandingColor">{outstandingContracts}</h3>
-                            <h3 className="font-bold text-2xl text-gray-700 inLine">&nbsp;Outstanding&nbsp;</h3>
-                            <h3 class="font-bold text-2xl inLine inProgressColor">{progressContracts}</h3>
-                            <h3 className="font-bold text-2xl text-gray-700 inLine">&nbsp;In Progress</h3>
-                            <div>
-                                <h4 class="font-bold text-2xl inLine text-gray-700">Average Contract Time:&nbsp;</h4>
-                                <h3 class="font-bold text-2xl inLine outstandingColor">{HundredContracts} Hours</h3>
-                            </div>
-                            <div>
-                                <h4 class="font-bold text-2xl inLine text-gray-700">MJ to and from Jita:&nbsp;</h4>
-                                <h3 class="font-bold text-2xl inLine outstandingColor">{MJJitaHundredContracts} Hours</h3>
-                            </div>
-                        </div>
-                        <p class="text-gray-600 text-justify">
-                            Contracts are issued directly to <span className="select-all">Evola Deliveries</span>.
-                            If you wish to use our services for a route that is currently not supported please contact <span className="font-bold">Nahtsu</span> directly.
-                            If you have any feedback please let us know!
-                        </p>
-                        <p>
-                            <span className="font-bold">Notice:</span> Most destinations should have couriers delivered within 48 hours.
-                        </p>
                     </div>
                     <hr />
                     <h2 className="w-full font-bold text-xl">Contract Creator</h2>
                     <h3 className="w-full font-bold text-gl">Select the Pickup and Dropoff</h3>
                     <div className="flex justify-between">
                         <label htmlFor="outbound">Pickup:</label>
-                        <select name="outbound" defaultValue={outboundValue} onChange={handleOutboundChanged} >
+                        <select name="outbound" defaultValue={outboundValue} onChange={handleOutboundChanged} disabled >
                             <option disabled hidden value=''></option>
                             {DataService.routes.map(route => (
                                 <optgroup key={route.region} label={route.region}>
@@ -131,7 +111,7 @@ export default function ContractCreator() {
                     </div>
                     <div className="flex justify-between">
                         <label htmlFor="inbound">Dropoff:</label>
-                        <select name="inbound" defaultValue="" onChange={handleInboundChanged} >
+                        <select name="inbound" defaultValue="" onChange={handleInboundChanged} disabled >
                             <option value="">None</option>
                             {DataService.getRoutes(outboundValue).map(route => (
                                 <optgroup key={route.region} label={route.region}>
@@ -147,11 +127,6 @@ export default function ContractCreator() {
                         <span className="select-all mx-2">{inboundRoute && inboundRoute.station}</span>
                     </div>
                     <PackageDetails system={inboundRoute} onPricingChange={handlePricingChange} />
-                </div>
-            </div>
-            <div className="my-1 px-1 w-full overflow-hidden sm:my-1 sm:px-1 sm:w-1/2 md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/2 xl:my-1 xl:px-1 xl:w-1/2">
-                <div className="flex justify-center w-full">
-                    <OrderTicket outbound={outboundRoute} inbound={inboundRoute} pricing={pricing} />
                 </div>
             </div>
         </div>
