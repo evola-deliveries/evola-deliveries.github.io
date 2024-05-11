c985446ed206d522eaf48c2b09c183f47a65c732
Yuri Lavelle <87241362+Nahtsu@users.noreply.github.com>
2024-05-11 15:18:16 +0100

    Update index.js

--- evola/src/components/main-page/index.js
+++ evola/src/components/main-page/index.js
@@ -11,7 +11,7 @@ const MainPage = () => {
 		<Router>
 			<div className="bg-cover flex flex-col min-h-screen" style={
 				{
-					"backgroundImage": "url(https://media.discordapp.net/attachments/812351111427129424/1238855555926458438/Original.webp?ex=6640cda5&is=663f7c25&hm=5fbfd0dba050782a8a98309b553ba75f17aaff470a4b15c6e97e5f9991fe7656&=&format=webp&width=1440&height=603)",
+					"backgroundImage": "url(https://cdn.discordapp.com/attachments/812351111427129424/1238857469162160228/evobgtest-min.png?ex=6640cf6d&is=663f7ded&hm=a871dc31e22626888e08d3e806e51289cc70201164e1062da3e606dcff8c3bd9&)",
 					"background-position": "center",
 					"background-repeat": "repeat",
 					"background-size": "cover"
