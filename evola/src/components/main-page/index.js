e4c57e113d1a7182afb70753800c0f9a7ed30520
dmportella <dmportella@users.noreply.github.com>
2024-05-13 16:51:02 +0000

    Update index.js

--- evola/src/components/main-page/index.js
+++ evola/src/components/main-page/index.js
@@ -11,7 +11,7 @@ const MainPage = () => {
 		<Router>
 			<div className="bg-cover flex flex-col min-h-screen" style={
 				{
-					"backgroundImage": "./public/background.png",
+					"backgroundImage": "./background.png",
 					"background-position": "center",
 					"background-repeat": "repeat",
 					"background-size": "cover"
