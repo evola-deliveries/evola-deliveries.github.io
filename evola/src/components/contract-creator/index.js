01b3e17abe95206ac0759a9dc361d525ccf60f3f
dmportella <dmportella@users.noreply.github.com>
2024-06-28 11:49:40 +0000

    added directus changes

--- evola/src/components/contract-creator/index.js
+++ evola/src/components/contract-creator/index.js
@@ -31,49 +31,63 @@ function calculatePricing(inboundRouteDeep, price, volume, janice) {
 }
 
 export default function ContractCreator() {
-    const [pricing, setPricing] = useState(calculatePricing(null, 0, 0, ""));
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [data, setData] = useState({});
 
-    const [outboundValue, setOutboundValue] = useState(DataService.default.region + "|" + DataService.default.system);
+    const [inboundValue, setInboundValue] = useState();
+    const [inboundRoute, setInboundRoute] = useState();
 
-    useEffect(() => {
-        setOutboundRoute(DataService.getOutboundRoute(outboundValue));
-    }, [outboundValue]);
-
-    const [inboundValue, setInboundValue] = useState("");
-
-    useEffect(() => {
-        setInboundRoute(DataService.getInboundRoute(outboundValue, inboundValue));
-    }, [inboundValue]); // eslint-disable-line react-hooks/exhaustive-deps
-    // TODO fix this
+    const [outboundValue, setOutboundValue] = useState();
+    const [outboundRoute, setOutboundRoute] = useState();
 
     const handleOutboundChanged = (event) => setOutboundValue(event.target.value);
     const handleInboundChanged = (event) => setInboundValue(event.target.value);
 
-    const [inboundRoute, setInboundRoute] = useState();
+    const [pricing, setPricing] = useState(calculatePricing(null, 0, 0, ""));
 
-    useEffect(() => {
-        setPricing(calculatePricing(inboundRoute, pricing.price, pricing.volume, pricing.janice));
-    }, [inboundRoute]); // eslint-disable-line react-hooks/exhaustive-deps
-
-    const [outboundRoute, setOutboundRoute] = useState();
-
-    const handlePricingChange = (price, volume, janice) => {
-        setPricing(calculatePricing(inboundRoute, price, volume, janice));
-    };
+    const handlePricingChange = (price, volume, janice) => setPricing(calculatePricing(inboundRoute, price, volume, janice));
 
     const [outstandingContracts, setOutstandingContracts] = useState();
     const [progressContracts, setProgressContracts] = useState();
     const [HundredContracts, setLast100ContractTime] = useState();
     const [MJJitaHundredContracts, setLast100MJJitaContracts] = useState();
 
-    ContractService.getContracts().then(r => {
-        setOutstandingContracts(r.Outstanding)
-        setProgressContracts(r.InProgress)
-        setLast100ContractTime(r.HundredContracts)
-        setLast100MJJitaContracts(r.MJJitaHundredContracts)
-    }).catch(reason => {
-        console.log(reason);
-    })
+    useEffect(() => {
+        if (outboundValue !== "" && outboundValue !== undefined) {
+            setOutboundRoute(DataService.getOutboundRoute(data, outboundValue));
+        }
+    }, [outboundValue, data]);
+
+    useEffect(() => {
+        setInboundRoute(DataService.getInboundRoute(data, outboundValue, inboundValue));
+    }, [inboundValue, data]); // eslint-disable-line react-hooks/exhaustive-deps
+    // TODO fix this
+
+    useEffect(() => {
+        setPricing(calculatePricing(inboundRoute, pricing.price, pricing.volume, pricing.janice));
+    }, [inboundRoute]); // eslint-disable-line react-hooks/exhaustive-deps
+
+
+    useEffect(() => {
+        DataService.getData().then(content => {
+            setData(content);
+            setOutboundValue(content.default.region + "|" + content.default.system)
+            setIsLoaded(true);
+        });
+
+        ContractService.getContracts().then(r => {
+            setOutstandingContracts(r.Outstanding)
+            setProgressContracts(r.InProgress)
+            setLast100ContractTime(r.HundredContracts)
+            setLast100MJJitaContracts(r.MJJitaHundredContracts)
+        }).catch(reason => {
+            console.log(reason);
+            setOutstandingContracts(0)
+            setProgressContracts(0)
+            setLast100ContractTime(0)
+            setLast100MJJitaContracts(0)
+        })
+    }, []);
 
     return (
         <div className="flex flex-wrap -mx-1 overflow-hidden sm:-mx-1 md:-mx-1 lg:-mx-1 xl:-mx-1">
@@ -81,29 +95,29 @@ export default function ContractCreator() {
                 <div className="shadow-lg rounded-lg bg-white px-2 py-2">
                     <div className="my-2">
                         <div className="headerCont">
-                            <h4 class="md:block text-2xl text-gray-400">WELCOME TO</h4>
-                            <h3 class="md:block font-bold text-3xl text-gray-700">EVOLA DELIVERIES</h3>
-                            <a class="flex items-baseline mt-3 text-3xl text-blue-600 hover:text-blue-900 focus:text-blue-900" href={ConfigService.discord_url} target="_blank" rel="noreferrer">
+                            <h4 className="md:block text-2xl text-gray-400">WELCOME TO</h4>
+                            <h3 className="md:block font-bold text-3xl text-gray-700">EVOLA DELIVERIES</h3>
+                            <a className="flex items-baseline mt-3 text-3xl text-blue-600 hover:text-blue-900 focus:text-blue-900" href={ConfigService.discord_url} target="_blank" rel="noreferrer">
                                 <span>Join Discord</span>
-                                <span class="text-xl ml-1">&#x279c;</span>
+                                <span className="text-xl ml-1">&#x279c;</span>
                             </a>
                         </div>
                         <div className="headerCont2">
-                            <h4 class="md:block text-2xl text-gray-400">Queue Status:</h4>
-                            <h3 class="font-bold text-2xl inLine outstandingColor">{outstandingContracts}</h3>
+                            <h4 className="md:block text-2xl text-gray-400">Queue Status:</h4>
+                            <h3 className="font-bold text-2xl inLine outstandingColor">{outstandingContracts}</h3>
                             <h3 className="font-bold text-2xl text-gray-700 inLine">&nbsp;Outstanding&nbsp;</h3>
-                            <h3 class="font-bold text-2xl inLine inProgressColor">{progressContracts}</h3>
+                            <h3 className="font-bold text-2xl inLine inProgressColor">{progressContracts}</h3>
                             <h3 className="font-bold text-2xl text-gray-700 inLine">&nbsp;In Progress</h3>
                             <div>
-                                <h4 class="font-bold text-2xl inLine text-gray-700">Average Contract Time:&nbsp;</h4>
-                                <h3 class="font-bold text-2xl inLine outstandingColor">{HundredContracts} Hours</h3>
+                                <h4 className="font-bold text-2xl inLine text-gray-700">Average Contract Time:&nbsp;</h4>
+                                <h3 className="font-bold text-2xl inLine outstandingColor">{HundredContracts} Hours</h3>
                             </div>
                             <div>
-                                <h4 class="font-bold text-2xl inLine text-gray-700">MJ to and from Jita:&nbsp;</h4>
-                                <h3 class="font-bold text-2xl inLine outstandingColor">{MJJitaHundredContracts} Hours</h3>
+                                <h4 className="font-bold text-2xl inLine text-gray-700">MJ to and from Jita:&nbsp;</h4>
+                                <h3 className="font-bold text-2xl inLine outstandingColor">{MJJitaHundredContracts} Hours</h3>
                             </div>
                         </div>
-                        <p class="text-gray-600 text-justify">
+                        <p className="text-gray-600 text-justify">
                             Contracts are issued directly to <span className="select-all">Evola Deliveries</span>.
                             If you have any feedback please let us know!
                         </p>
@@ -118,13 +132,13 @@ export default function ContractCreator() {
                         <label htmlFor="outbound">Pickup:</label>
                         <select name="outbound" defaultValue={outboundValue} onChange={handleOutboundChanged} >
                             <option disabled hidden value=''></option>
-                            {DataService.routes.map(route => (
-                                <optgroup key={route.region} label={route.region}>
+                            {isLoaded ? data.routes.map(route => {
+                                return <optgroup key={route.region} label={route.region}>
                                     {route.systems.map(system => (
                                         <option value={route.region + "|" + system.system}>{system.system}</option>
                                     ))}
                                 </optgroup>
-                            ))}
+                            }) : null}
                         </select>
                     </div>
                     <div className='flex justify-between my-2 border-b-2 border-dashed border-green-700'>
@@ -140,13 +154,13 @@ export default function ContractCreator() {
                         <label htmlFor="inbound">Dropoff:</label>
                         <select name="inbound" defaultValue="" onChange={handleInboundChanged} >
                             <option value="">None</option>
-                            {DataService.getRoutes(outboundValue).map(route => (
-                                <optgroup key={route.region} label={route.region}>
+                            {isLoaded ? DataService.getRoutes(data, outboundValue).map(route => {
+                                return <optgroup key={route.region} label={route.region}>
                                     {route.systems.map(system => (
                                         <option value={route.region + "|" + system.system}>{system.system}</option>
                                     ))}
                                 </optgroup>
-                            ))}
+                            }) : null}
                         </select>
                     </div>
                     <div className='flex justify-between my-2 border-b-2 border-dashed border-green-700'>
--- evola/src/services/data-service/index.js
+++ evola/src/services/data-service/index.js
@@ -1,19 +1,38 @@
-import Data from './data.json';
+import Config from '../config-service';
 import { find } from 'lodash';
 
 const service = {
-    routes: Data.routes,
-    default: Data.default,
-    getRoutes(outbound) {
+    async getData() {
+        const config = {
+            method: 'GET',
+            cache: 'no-cache',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'text/plain',
+                'accept': 'application/json'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer'
+        };
+
+        return await fetch(Config.directus_url, config).then(response => {
+            const contentType = response.headers.get('content-type');
+            if (!contentType || !contentType.includes('application/json')) {
+                throw new TypeError("Oops, we haven't got JSON!");
+            }
+            return response.json();
+        })
+    },
+    getRoutes(data, outbound) {
         const regionName = outbound.split('|')[0];
         const systemName = outbound.split('|')[1];
     
-        const region = find(this.routes, (value) => value.region === regionName);
+        const region = find(data.routes, (value) => value.region === regionName);
         const system = find(region.systems, (value) => value.system === systemName);
     
         return system.regions;
     },
-    getInboundRoute(outbound, inbound) {
+    getInboundRoute(data, outbound, inbound) {
         if (outbound === "" || outbound === undefined) return;
         if (inbound === "" || inbound === undefined) return;
 
@@ -23,7 +42,7 @@ const service = {
         const inboundRegionName = inbound.split('|')[0];
         const inboundSystemName = inbound.split('|')[1];
     
-        const outboundRegion = find(this.routes, (value) => value.region === outboundRegionName);
+        const outboundRegion = find(data.routes, (value) => value.region === outboundRegionName);
         const outboundSystem = find(outboundRegion.systems, (value) => value.system === outboundSystemName);
 
         const inboundRegion = find(outboundSystem.regions, (value) => value.region === inboundRegionName);
@@ -34,11 +53,11 @@ const service = {
 
         return inboundSystem;
     },
-    getOutboundRoute(outbound) {
+    getOutboundRoute(data, outbound) {
         const regionName = outbound.split('|')[0];
         const systemName = outbound.split('|')[1];
     
-        const region = find(this.routes, (value) => value.region === regionName);
+        const region = find(data.routes, (value) => value.region === regionName);
         const system = find(region.systems, (value) => value.system === systemName);
     
         return system;
