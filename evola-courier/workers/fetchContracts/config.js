c96e3bc6b9055585a8780660278a76ec1a6392bc
Gothyl <dmportella@users.noreply.github.com>
2025-07-01 19:04:01 +0000

    process everything is a boolean

--- evola-courier/workers/fetchContracts/config.js
+++ evola-courier/workers/fetchContracts/config.js
@@ -1,8 +1,8 @@
 import evolaConfig from '../../shared/config.js';
 
 const config = {
-    process_everything: process.env.PROCESS_EVERYTHING,
-    change_interval_seconds: process.env.CHANGE_INTERVAL_SECONDS,
+    process_everything: String(process.env.PROCESS_EVERYTHING).toLowerCase() === 'true',
+    change_interval_seconds: parseInt(process.env.CHANGE_INTERVAL_SECONDS || '86400', 10),
     ...evolaConfig
 }
 
