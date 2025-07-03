09c3db45fede198c18a80d4122ca296ed0e7cc86
Gothyl <dmportella@users.noreply.github.com>
2025-07-03 16:18:36 +0000

    trigger schedule on repeat

--- evola-courier/dispatchers/trigger-fetch/trigger-fetch.js
+++ evola-courier/dispatchers/trigger-fetch/trigger-fetch.js
@@ -1,10 +1,22 @@
+// scheduler/fetch-contracts-daily.js
+
 import { fetchContracts } from '../../shared/queue.js';
 import { createEventPayload } from '../../shared/utils/createEventPayload.js';
+import config from './config.js';
 
 (async () => {
 	const newJob = createEventPayload();
+
 	await fetchContracts.add('start', newJob, {
+		jobId: 'daily-fetch-contracts',
+		repeat: {
+			cron: config.trigger_fetch_cron
+		},
+		removeOnComplete: true,
+		removeOnFail: true,
 		attempts: 3,
 		backoff: { type: 'exponential', delay: 3000 }
 	});
+
+	console.log('[fetch-contracts-daily] Scheduled fetchContracts job daily at 04:00 UTC');
 })();
